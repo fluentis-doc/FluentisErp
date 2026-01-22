7c55ee8c6a715880bd563d9dcde4a32e37f022db
rodica1234 <rodica.u@fluentis.com>
2026-01-22 09:13:52 +0100

    restore config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -388,7 +388,7 @@ const config = {
 
         // The maximum number of search results shown to the user. This does _not_ affect performance of
         // searches, but simply does not display additional search results that have been found.
-        maxSearchResults: 50,
+        maxSearchResults: 64,
 
         // lunr.js-specific settings
         lunr: {
@@ -398,27 +398,27 @@ const config = {
           // By default, it splits the text at whitespace and dashes.
           //
           // Note: Does not work for "ja" and "th" languages, since these use a different tokenizer.
-          tokenizerSeparator: /[\s\-_]+|(?=[A-Z])/,
+          tokenizerSeparator: /[\s\-]+/,
           // https://lunrjs.com/guides/customising.html#similarity-tuning
           //
           // This parameter controls the importance given to the length of a document and its fields. This
           // value must be between 0 and 1, and by default it has a value of 0.75. Reducing this value
           // reduces the effect of different length documents on a term’s importance to that document.
-          b: 0.6,
+          b: 0.75,
           // This controls how quickly the boost given by a common word reaches saturation. Increasing it
           // will slow down the rate of saturation and lower values result in quicker saturation. The
           // default value is 1.2. If the collection of documents being indexed have high occurrences
           // of words that are not covered by a stop word filter, these words can quickly dominate any
           // similarity calculation. In these cases, this value can be reduced to get more balanced results.
-          k1: 1,
+          k1: 1.2,
           // By default, we rank pages where the search term appears in the title higher than pages where
           // the search term appears in just the text. This is done by "boosting" title matches with a
           // higher value than content matches. The concrete boosting behavior can be controlled by changing
           // the following settings.
-      titleBoost: 15,      // TITOLI = fondamentali
-      contentBoost: 2,     // testo normale
-      tagsBoost: 6,        // usa tags nei doc!
-      parentCategoriesBoost: 4, // Only used when indexDocSidebarParentCategories > 0
+          titleBoost: 5,
+          contentBoost: 1,
+          tagsBoost: 3,
+          parentCategoriesBoost: 2, // Only used when indexDocSidebarParentCategories > 0
         }
       },
     ],
